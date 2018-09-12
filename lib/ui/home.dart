9f74681fbf4ec30550b65aa1173e5eb22495f4a0
Welington Gadelha <gadelha.welington@gmail.com>
2018-09-12 14:55:05 -0400

    Alterações do Json Home, imagens.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:previsao_tempo/ui/cidades.dart';
+import 'dart:convert';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
 
 class Home extends StatefulWidget {
   @override
@@ -8,10 +11,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  List<Previsaotempo> listObjPrevisao = new List<Previsaotempo>();
   String cidadeFavorita;
 
   @override
   void initState() {
+    carregarPrevisao();
     super.initState();
     print("ok");
     if (cidadeFavorita == null || cidadeFavorita.isEmpty) {
@@ -21,65 +26,41 @@ class _HomeState extends State<Home> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.lightGreen[900],
-          centerTitle: true,
-          title: Text("Previsão do Tempo $cidadeFavorita",
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
-          bottom:
-
-          TabBar(
-            labelColor: Colors.amber,
-            unselectedLabelColor: Colors.white,
-            tabs: [
-              Tab(
-                child: Text("Hoje"),
-              ),
-              Tab(
-                child: Text("12/09/2018"),
-              ),
-              Tab(
-                child: Text("13/09/2018"),
-              ),
-            ],
-          ),
-        ),
-        body:
-        Container( child:
-        TabBarView(
-          children: [
-            pagePrevisaoTempo(
-                "SEGUNDA-FEIRA",
-                "36",
-                "25",
-                "Claro a parcialmente nublado com possibilidade de chuva em áreas isoladas",
-                "60",
-                "20",
-                "N-NW",
-                "Fracos")
-          ],
-        ),
-        ),
-
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _selecionaCidade(context);
-          },
-          backgroundColor: Colors.lightGreen[900],
-          tooltip: 'Inbox',
-          child: Icon(Icons.location_on),
-        ),
-      ),
-    );
+    return getPageHomeDados(context);
   }
+
+  //CARREGAR INFORMAÇÕES DO JSON DE PREVISÃO
+  Future<String> _loadCrosswordAsset() async {
+    return await rootBundle.loadString('assets/data/previsaoTempo.json');
+  }
+
+  Future<Map> loadCrossword() async {
+    Map decoded = json.decode(await _loadCrosswordAsset());
+    return decoded;
+  }
+
+  void carregarPrevisao() async {
+    Map decoded = await loadCrossword();
+    for (var dias in decoded["dias"]) {
+      listObjPrevisao.add(Previsaotempo(
+          dias["data"],
+          dias["nomeCidade"],
+          dias["tempo"],
+          dias["temperaturaMax"],
+          dias["temperaturaMin"],
+          dias["umidadeMax"],
+          dias["umidadeMin"],
+          dias["direcaoVentos"],
+          dias["intensidade"],
+          dias["diaSemana"]));
+    }
+    setState(() {
+      this.listObjPrevisao = listObjPrevisao;
+    });
+  }
+  //FIM CARREGAR INFORMAÇÕES DO JSON DE PREVISÃO
 
   void _selecionaCidade(BuildContext c) async {
     final _recCidade = await Navigator.push(c,
@@ -95,10 +76,65 @@ class _HomeState extends State<Home> {
     }
   }
 
+  //WIDGET QUE CONSTROI A PAGINA INICIAL COM OS DADOS DO JSON
+  Widget getPageHomeDados(BuildContext context) {
+    // PEGANDO A DATA ATUAL E COMPARANDO COM A DO PRIMEIRO OBJETO DA LISTA
+    String hoje;
+    var data = DateTime.now();
+    if(listObjPrevisao[0].data == "${data.day}/0${data.month}/${data.year}"){
+      hoje = "Hoje";
+    }
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen[900],
+          centerTitle: true,
+          title: Text("Previsão do Tempo $cidadeFavorita",
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+          bottom:
+          TabBar(
+            labelColor: Colors.amber,
+            unselectedLabelColor: Colors.white,
+            tabs: [
+              Tab(
+                child: Text(hoje),
+              ),
+              Tab(
+                child: Text(listObjPrevisao[1].data),
+              ),
+              Tab(
+                child: Text(listObjPrevisao[2].data),
+              ),
+            ],
+          ),
+        ),
+        body:
+        Container( child:
+        TabBarView(
+          children: [
+            pagePrevisaoTempo(listObjPrevisao[0], "01"),
+            pagePrevisaoTempo(listObjPrevisao[1], "02"),
+            pagePrevisaoTempo(listObjPrevisao[2], "03"),
+          ],
+        ),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _selecionaCidade(context);
+          },
+          backgroundColor: Colors.lightGreen[900],
+          tooltip: 'Inbox',
+          child: Icon(Icons.location_on),
+        ),
+      ),
+    );
+  }
 }
 
-Widget pagePrevisaoTempo(String diaSemana, String max, String min, String tempo,
-    String umidadeMax, String umidadeMin, String direcao, String intensidade) {
+Widget pagePrevisaoTempo(Previsaotempo objPrevTemp, String imgTemp) {
   return Container(
     decoration: new BoxDecoration(
       color: Colors.black87,
@@ -108,20 +144,19 @@ Widget pagePrevisaoTempo(String diaSemana, String max, String min, String tempo,
       ),
     ),
     child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          LinhaTemperatura("$diaSemana", "$max", "$min"),
-          LinhaTempo("$tempo"),
-          LinhaUmidade("$umidadeMax", "$umidadeMin"),
-          LinhaVentos("$direcao", "$intensidade")
+          LinhaTemperatura(objPrevTemp.diaSemana, objPrevTemp.temperaturaMax, objPrevTemp.temperaturaMin, "$imgTemp"),
+          LinhaTempo(objPrevTemp.tempo),
+          LinhaUmidade(objPrevTemp.umidadeMax, objPrevTemp.umidadeMin),
+          LinhaVentos(objPrevTemp.direcaoVentos, objPrevTemp.intensidade)
         ],
       ),
   );
 }
 
-Widget LinhaTemperatura(String diaSemana, String max, String min) {
+Widget LinhaTemperatura(String diaSemana, String tempMax, String tempMin, String imgTempo) {
   return Padding(
-    padding: EdgeInsets.only(left: 20.0, top: 5.0, right: 20.0, bottom: 0.0),
+    padding: EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 0.0),
     child: Column(
       children: <Widget>[
         TituloELinha("$diaSemana"),
@@ -131,7 +166,7 @@ Widget LinhaTemperatura(String diaSemana, String max, String min) {
               padding: EdgeInsets.only(
                   left: 0.0, top: 0.0, right: 15.0, bottom: 0.0),
               child: Image.asset(
-                "images/icons/01.png",
+                "images/icons/$imgTempo.png",
                 fit: BoxFit.cover,
                 height: 115.0,
               ),
@@ -147,7 +182,7 @@ Widget LinhaTemperatura(String diaSemana, String max, String min) {
                   ),
                 ),
                 Text(
-                  "$max°C - $min°C",
+                  "$tempMax°C - $tempMin°C",
                   style: TextStyle(
                       fontSize: 35.0,
                       fontWeight: FontWeight.bold,
@@ -164,7 +199,7 @@ Widget LinhaTemperatura(String diaSemana, String max, String min) {
 
 Widget LinhaTempo(String tempo) {
   return Padding(
-    padding: EdgeInsets.only(left: 20.0, top: 5.0, right: 20.0, bottom: 0.0),
+    padding: EdgeInsets.only(left: 20.0, top: 15.0, right: 20.0, bottom: 0.0),
     child: Column(
       children: <Widget>[
         TituloELinha("TEMPO"),
@@ -178,13 +213,13 @@ Widget LinhaTempo(String tempo) {
   );
 }
 
-Widget LinhaUmidade(String max, String min) {
+Widget LinhaUmidade(String umidadeMax, String umidadeMin) {
   return Padding(
-    padding: EdgeInsets.only(left: 20.0, top: 5.0, right: 20.0, bottom: 0.0),
+    padding: EdgeInsets.only(left: 20.0, top: 15.0, right: 20.0, bottom: 0.0),
     child: Column(
       children: <Widget>[
         TituloELinha("UMIDADE MÁX E MÍN"),
-        Descricao("$max% - $min%", "umidade"),
+        Descricao("$umidadeMax% - $umidadeMin%", "umidade"),
       ],
     ),
   );
@@ -192,11 +227,11 @@ Widget LinhaUmidade(String max, String min) {
 
 Widget LinhaVentos(String direcao, String intensidade) {
   return Padding(
-    padding: EdgeInsets.only(left: 20.0, top: 5.0, right: 20.0, bottom: 0.0),
+    padding: EdgeInsets.only(left: 20.0, top: 15.0, right: 20.0, bottom: 0.0),
     child: Column(
       children: <Widget>[
         TituloELinha("VENTOS - DIREÇÃO E INTENSIDADE"),
-        Descricao("$direcao/$intensidade", "ventos"),
+        Descricao("$direcao/$intensidade", "vazia"),
       ],
     ),
   );
@@ -204,19 +239,32 @@ Widget LinhaVentos(String direcao, String intensidade) {
 // WIDGET QUE MONTA A OS DESCRIÇÃO DE CADA MOSTRAGEM
 
 Widget Descricao(String descricao, String img) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: <Widget>[
-      Text(
-        "$descricao",
-        style: TextStyle(fontSize: 30.0, color: Colors.white),
-      ),
-      Image.asset(
-        "images/icons/$img.png",
-        height: 25.0,
-      ),
-    ],
-  );
+  if(img == "vazia"){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          "$descricao",
+          style: TextStyle(fontSize: 30.0, color: Colors.white),
+        ),
+      ],
+    );
+  } else {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          "$descricao",
+          style: TextStyle(fontSize: 30.0, color: Colors.white),
+        ),
+        Image.asset(
+          "images/icons/$img.png",
+          height: 25.0,
+        ),
+      ],
+    );
+  }
+
 }
 
 // WIDGET QUE MONTA A OS TITULOS DE CADA MOSTRAGEM
@@ -226,7 +274,7 @@ Widget TituloELinha(String titulo) {
     children: <Widget>[
       Text(
         "$titulo",
-        style: TextStyle(fontSize: 20.0, color: Colors.white),
+        style: TextStyle(fontSize: 19.5, color: Colors.white, fontWeight: FontWeight.bold),
       ),
       Container(
         color: Colors.white.withOpacity(0.85),
@@ -235,4 +283,29 @@ Widget TituloELinha(String titulo) {
       ),
     ],
   );
+}
+
+class Previsaotempo {
+  String data;
+  String nomeCidade;
+  String tempo;
+  String temperaturaMax;
+  String temperaturaMin;
+  String umidadeMax;
+  String umidadeMin;
+  String direcaoVentos;
+  String intensidade;
+  String diaSemana;
+
+  Previsaotempo(
+      this.data,
+      this.nomeCidade,
+      this.tempo,
+      this.temperaturaMax,
+      this.temperaturaMin,
+      this.umidadeMax,
+      this.umidadeMin,
+      this.direcaoVentos,
+      this.intensidade,
+      this.diaSemana);
 }
